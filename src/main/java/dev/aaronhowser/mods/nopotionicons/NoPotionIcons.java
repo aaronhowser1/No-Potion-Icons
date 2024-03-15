79d1875b52ac84857ba4c175b403eee194022982
Aaron Howser <aaronhowser1@gmail.com>
2024-03-14 22:42:57 -0500

    Changed to using a static class with value instead of using @OnlyIn

--- src/main/java/dev/aaronhowser/mods/nopotionicons/NoPotionIcons.java
+++ src/main/java/dev/aaronhowser/mods/nopotionicons/NoPotionIcons.java
@@ -1,24 +1,31 @@
 package dev.aaronhowser.mods.nopotionicons;
 
 import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.client.event.RenderGameOverlayEvent;
 import net.minecraftforge.client.gui.ForgeIngameGui;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
-@Mod("nopotionicons")
+@Mod(NoPotionIcons.MODID)
 public class NoPotionIcons {
+
+    static final String MODID = "nopotionicons";
+
     public NoPotionIcons() {
         MinecraftForge.EVENT_BUS.register(this);
     }
 
-    @SubscribeEvent
-    @OnlyIn(Dist.CLIENT)
-    public void onRenderGuiEvent(RenderGameOverlayEvent.PreLayer event) {
-        if (event.getOverlay() == ForgeIngameGui.POTION_ICONS_ELEMENT) {
-            event.setCanceled(true);
+    @Mod.EventBusSubscriber(
+            modid = MODID,
+            bus = Mod.EventBusSubscriber.Bus.MOD,
+            value = Dist.CLIENT)
+    public static class ClientModEvents {
+        @SubscribeEvent
+        public void onRenderGuiEvent(RenderGameOverlayEvent.PreLayer event) {
+            if (event.getOverlay() == ForgeIngameGui.POTION_ICONS_ELEMENT) {
+                event.setCanceled(true);
+            }
         }
     }
 
