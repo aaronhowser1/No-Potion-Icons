a07293969e5719231c93ced0ebb1f13038f76ec7
Aaron Howser <aaronhowser1@gmail.com>
2024-03-14 22:06:49 -0500

    Moved things to dev.aaronhowser.

--- src/main/java/dev/aaronhowser/nopotionicons/NoPotionIcons.java
+++ src/main/java/dev/aaronhowser/nopotionicons/NoPotionIcons.java
@@ -1,4 +1,4 @@
-package com.example.examplemod;
+package dev.aaronhowser.nopotionicons;
 
 import com.mojang.logging.LogUtils;
 import net.minecraft.world.level.block.Block;
@@ -17,14 +17,13 @@ import org.slf4j.Logger;
 
 import java.util.stream.Collectors;
 
-// The value here should match an entry in the META-INF/mods.toml file
-@Mod("examplemod")
-public class ExampleMod
+@Mod("nopotionicons")
+public class NoPotionIcons
 {
     // Directly reference a slf4j logger
     private static final Logger LOGGER = LogUtils.getLogger();
 
-    public ExampleMod()
+    public NoPotionIcons()
     {
         // Register the setup method for modloading
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
@@ -47,7 +46,7 @@ public class ExampleMod
     private void enqueueIMC(final InterModEnqueueEvent event)
     {
         // Some example code to dispatch IMC to another mod
-        InterModComms.sendTo("examplemod", "helloworld", () -> { LOGGER.info("Hello world from the MDK"); return "Hello world";});
+        InterModComms.sendTo("nopotionicons", "helloworld", () -> { LOGGER.info("Hello world from the MDK"); return "Hello world";});
     }
 
     private void processIMC(final InterModProcessEvent event)
