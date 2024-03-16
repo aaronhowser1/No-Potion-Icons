63c9db774f16047194672c23ff0f57d24aa5bee8
Aaron Howser <aaronhowser1@gmail.com>
2024-03-15 22:40:17 -0500

    It can now be toggled

--- src/main/java/dev/aaronhowser/mods/nopotionicons/NoPotionIcons.java
+++ src/main/java/dev/aaronhowser/mods/nopotionicons/NoPotionIcons.java
@@ -1,12 +1,18 @@
 package dev.aaronhowser.mods.nopotionicons;
 
+import com.mojang.blaze3d.platform.InputConstants;
+import net.minecraft.client.KeyMapping;
 import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.client.event.InputEvent;
+import net.minecraftforge.client.event.RegisterKeyMappingsEvent;
 import net.minecraftforge.client.event.RenderGuiOverlayEvent;
 import net.minecraftforge.client.gui.overlay.GuiOverlayManager;
 import net.minecraftforge.client.gui.overlay.VanillaGuiOverlay;
+import net.minecraftforge.client.settings.KeyConflictContext;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
+import org.lwjgl.glfw.GLFW;
 
 @Mod(NoPotionIcons.MODID)
 public class NoPotionIcons {
@@ -16,18 +22,51 @@ public class NoPotionIcons {
         MinecraftForge.EVENT_BUS.register(this);
     }
 
+    public static final KeyMapping TOGGLE_POTION_ICONS = new KeyMapping(
+            "key.nopotionicons.toggle_potion_icons",
+            KeyConflictContext.IN_GAME,
+            InputConstants.Type.KEYSYM,
+            GLFW.GLFW_KEY_COMMA,
+            "key.categories.ui"
+    );
+
     @Mod.EventBusSubscriber(
             modid = MODID,
             value = Dist.CLIENT
     )
     public static class ClientModEvents {
 
+        public static boolean hidePotionIcons = true;
+
         @SubscribeEvent
         public static void onRenderGuiEvent(RenderGuiOverlayEvent.Pre event) {
+            if (!hidePotionIcons) return;
+
             if (event.getOverlay() == GuiOverlayManager.findOverlay(VanillaGuiOverlay.POTION_ICONS.id())) {
                 event.setCanceled(true);
             }
         }
 
+        @SubscribeEvent
+        public static void onKeyInputEvent(InputEvent.Key event) {
+            if (TOGGLE_POTION_ICONS.consumeClick()) {
+                hidePotionIcons = !hidePotionIcons;
+            }
+        }
+
+    }
+
+    @Mod.EventBusSubscriber(
+            modid = MODID,
+            value = Dist.CLIENT,
+            bus = Mod.EventBusSubscriber.Bus.MOD
+    )
+    public static class ClientModBusEvents {
+
+
+        @SubscribeEvent
+        public static void onKeyRegister(RegisterKeyMappingsEvent event) {
+            event.register(TOGGLE_POTION_ICONS);
+        }
     }
 }
